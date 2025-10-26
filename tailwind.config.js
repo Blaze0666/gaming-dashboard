048a409363e32b92cfe782f644a0bd5596037389
Blaze0666 <jsanderson1985@yahoo.com>
2025-10-25 22:16:30 -0600

    Force keep all Tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,17 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.{html,js}",
+    "./src/index.css"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
+  // KEEP ALL CLASSES IN PRODUCTION
+  safelist: [
+    {
+      pattern: /./ // <-- This keeps EVERY Tailwind class
+    }
+  ]
 }
